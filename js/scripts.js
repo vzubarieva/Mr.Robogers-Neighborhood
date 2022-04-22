fe913cc2a2507321852e69a2eb29164e5db63e81
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-22 00:20:25 -0700

    add implementation of first test of business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// Bussines logic
+// Business logic
+
+function beepBoop(number) {
+  const parsedNumber = parseInt(number);
+  const result = [];
+  for (let index = 0; index < parsedNumber; index += 1) {
+    result.push(index);
+  }
+
+  return result;
+}
